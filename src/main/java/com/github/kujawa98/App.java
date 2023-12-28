027d105b7948b7899f2c57d98f2a6a59e22c0597
Kacper Kujawa <kacperkujawa@macbook-pro-kacper.local>
2023-12-28 10:31:15 +0100

    Initial commit

---
+++ src/main/java/com/github/kujawa98/App.java
@@ -0,0 +1,13 @@
+package com.github.kujawa98;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
